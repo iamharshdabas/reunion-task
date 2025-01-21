fbca96e8e39c44516a6c9cb5bbb9a0ae909a9572
Harsh Dabas <iamharshdabas@gmail.com>
2025-01-21 23:39:41 +0530

    fix: next config env path

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import createJiti from "jiti";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
-jiti("./app/env");
+jiti("./lib/env");
 
 const nextConfig: NextConfig = {
   // for faster build
